3e4b73c3a15f2bd9648360560f21af1f6ee6481b
Cassie DiBenedetti <6212cassiedib@gmail.com>
2022-01-17 16:46:26 -0500

    Adding friday_jan14_2.py to dtsc_class2

---
+++ week1/friday_jan14_2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def multifunc(a,b) -> int:
+    """A function that returns the product of two integers
+
+    Args:
+        a (int): the first integer
+        b (int): the second integer
+
+    Returns:
+        int: the product of integers a and b
+    """
+    return a * b
+
+print(multifunc(3,7))
