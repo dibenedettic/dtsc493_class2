df78eecfdfe4b9424f5e35dd0318f5670e8e2cd7
Cassie DiBenedetti <6212cassiedib@gmail.com>
2022-01-17 16:39:55 -0500

    Adding friday_jan14.py to dtsc_class2

---
+++ week1/friday_jan14.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def sumfunc(a,b) -> int:
+    """A function that returns the sum of two integers
+
+    Args:
+        a (int): the first integer
+        b (int): the second integer
+
+    Returns:
+        int: the sum of integers a and b
+    """
+    return a + b
+
+print(sumfunc(1,5))
